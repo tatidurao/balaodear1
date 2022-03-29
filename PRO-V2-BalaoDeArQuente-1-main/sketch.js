46b9c9e6b86a7966f1f40245aa92a0c64a3d4c52
tatidurao <91564423+tatidurao@users.noreply.github.com>
2022-03-29 19:52:32 -0300

    Add files via upload

---
+++ PRO-V2-BalaoDeArQuente-1-main/sketch.js
@@ -0,0 +1,42 @@
+var bg, bgImg
+var bottomGround
+var topGround
+var balloon, balloonImg
+var obstacleTop, obsTop1, obsTop2
+var obstacleBottom, obsBottom1, obsBottom2, obsBottom3
+
+
+function preload(){
+
+}
+
+function setup(){
+
+//imagem de plano de fundo
+
+
+//criando canto superior e inferior
+
+
+
+      
+//criando o balão     
+
+
+
+
+}
+
+function draw() {
+  
+  background("black");
+        
+          //fazendo o balão de ar quente pular
+          
+
+          //adicionando gravidade
+           
+   
+        drawSprites();
+        
+}
